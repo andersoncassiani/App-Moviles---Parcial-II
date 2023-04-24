39d429595fd3df6674aaad0b29cc7948a4f2df7d
andersoncz <anders_cz@outlook.com>
2023-04-24 13:55:29 -0500

    Finalizacion de la App, funcionando.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,16 @@ class NinjaCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/perfil.jpg'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
             Text('Nombre',
                 style: TextStyle(
                   color: Colors.black,
@@ -45,6 +55,92 @@ class NinjaCard extends StatelessWidget {
                     letterSpacing: 2.0,
                     fontSize: 28.0,
                     fontWeight: FontWeight.bold)),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'ander.cassiani@unitecnar.edu',
+                  style: TextStyle(
+                      color: Colors.blue, fontSize: 18.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  '+573003444567',
+                  style: TextStyle(
+                      color: Colors.blue, fontSize: 18.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.location_city_sharp,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'Unitecnar',
+                  style: TextStyle(
+                      color: Colors.blue, fontSize: 18.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.computer,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'Desarrollador de Software',
+                  style: TextStyle(
+                      color: Colors.blue, fontSize: 18.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              height: 33.0,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  border: Border.all(color: Colors.black, width: 2.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[850]!.withOpacity(0.29),
+                        blurRadius: 10)
+                  ]),
+              child: const Center(
+                child: Text(
+                  '📝 Modificar mi perfil',
+                  style: TextStyle(fontSize: 22.0, color: Colors.white),
+                ),
+              ),
+            ),
           ],
         ),
       ),
